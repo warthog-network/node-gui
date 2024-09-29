5f8635adf98476fa01da92559b1a8939a422ae33
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-09-29 15:01:14 -0400

    thanks for the catch

---
+++ react-node-gui/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-yellow-200'>
+      node-gui
+    </div>
+  )
+}
+
+export default App
